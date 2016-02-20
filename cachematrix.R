4205f132320bb45dcff01685125e14428d3367d7
Alan95649 <alan.c.wilkening@gmail.com>
2016-02-20 17:06:01 -0500

    Matrix Cache WH done
    Completed matrix caching home work.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions calculate and cache a matrix inversion 
+## Coded 2/20/16 as part of Coursera R class, week 3.
 
-## Write a short comment describing this function
-
+# This function starts it all, user should give it a nice
+#  square invertable matrix and save the output to be used
+#  with cacheSolve()
 makeCacheMatrix <- function(x = matrix()) {
+    ## Return a matrix that is the inverse of 'x'
+    mat <- NULL # ensure cached matrix is empty
+    set <- function(y) {
+        x <<- y
+        mat <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inv) mat <<- inv
+    getInv <- function() mat
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 
 }
 
-
-## Write a short comment describing this function
-
+# Called by user after makeCacheMatrix with special vector 'x'
+#  checks to see if mat-inv has already been calcuated, if so 
+#  the last mat-inv is returned.  Else a new mat-inv is found 
+#  and returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getInv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInv(m)
+    m
 }
